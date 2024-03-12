2ec6656c593f71f47736a4253b2e88ade4f42fb3
Ashish <ashish.ashish05@outlook.com>
2024-03-12 22:27:19 +0530

    added campus discussion forum frontend

---
+++ src/constants/data.js
@@ -0,0 +1,7 @@
+export const categories = [
+    { id: 1, type: "Coding" },
+    { id: 2, type: "DSA" },
+    { id: 3, type: "Database" },
+    { id: 4, type: "Tech" },
+    { id: 5, type: "Others" }
+];
